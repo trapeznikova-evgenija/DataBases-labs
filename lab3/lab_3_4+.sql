a1afff41d8a6033bfc3883ff84f47ff556cf3219
trapeznikova-evgenija <trapeznikova.evg@yandex.ru>
2018-03-22 12:24:45 +0300

    Добавлена 3+

---
+++ lab3/lab_3_4+.sql
@@ -0,0 +1,47 @@
+/* 1 */
+
+SELECT *
+FROM child
+ORDER BY name ASC;
+
+SELECT *
+FROM child
+ORDER BY adress DESC;
+
+SELECT *
+FROM child
+ORDER BY name, date_of_birth ASC;
+
+SELECT
+  name,
+  date_of_birth
+FROM child
+ORDER BY name ASC;
+
+/* 2 */
+
+SELECT MIN(id_innoculation)
+FROM vaccinated;
+
+SELECT MAX(date_vaccination)
+FROM vaccinated;
+
+SELECT AVG(id_type)
+FROM type;
+
+SELECT SUM(id_type)
+FROM innoculation;
+
+/* 3 */
+
+/* количество прививок по каждому типу вакцинации */
+SELECT COUNT(id), id_type
+FROM innoculation
+GROUP BY id_type;
+
+/* выбирает прививки, которые ставятся одним типом вакцинации больше одного раза */
+SELECT COUNT(id), id_type
+FROM innoculation
+GROUP BY id_type
+HAVING COUNT(id) > 1;
+
